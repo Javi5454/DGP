fdf7dac3baba0ede57831e7aa28c322336df3a92
Clara <cbolivarpelaez@gmail.com>
2024-12-04 20:23:05 +0100

    Intentando subir el registro

--- src/pictograms/forms.py
+++ src/pictograms/forms.py
@@ -4,8 +4,13 @@ from logic.models import Person
 class PersonalDataForm(forms.Form):
     first_name = forms.CharField(label='Nombre', max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(label='Apellidos', max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    profile_picture = forms.ImageField(label='Foto de Perfil', widget=forms.FileInput(attrs={'class': 'form-control-file'}))
-
+    profile_picture = forms.ImageField(
+        label='Foto de Perfil', 
+        widget=forms.FileInput(attrs={
+            'class': 'form-control-file',
+            'id': 'id_picture'  # Añadimos el ID necesario para que coincida con el script
+        })
+    )
 class PictogramPasswordForm(forms.Form):
     pictogram_sequence = forms.CharField(
         widget=forms.HiddenInput(),
